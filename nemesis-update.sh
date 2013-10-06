d37c573262416fc03becc8036b4f98dc103c8237
x684867 <mail@samcaldwell.net>
2013-10-05 20:40:24 -0500

    Added nemesis-install.sh script (unfinished)

--- nemesis-update.sh
+++ nemesis-update.sh
@@ -6,20 +6,5 @@
 [ "$(whoami)" != "root" ] && echo "This script must be run as root." && exit 1
 
 echo "Updating broker.nemesiscloud.com"
-cd /var/www/broker.nemesiscloud.com
-git pull broker master
-
-echo "Updating cipher.nemesiscloud.com"
-cd /var/www/cipher.nemesiscloud.com
-git pull cipher master
-
-echo "Updating keys.nemesiscloud.com"
-cd /var/www/keys.nemesiscloud.com
-git pull cipher master
-
-echo "Updating audit.nemesiscloud.com"
-cd /var/www/audit.nemesiscloud.com
-git pull audit master
-
-echo "done."
-exit 0
+cd /var/www/
+git pull nemesis_server master
