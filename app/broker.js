e2f188217a9b53002fc2b862ccdbc3d6583390cf
x684867 <mail@samcaldwell.net>
2013-10-06 21:48:28 -0500

    Trial code for the node.js application

--- app/broker.js
+++ app/broker.js
@@ -1,4 +1,15 @@
-
+/*
+	/srv/nemesis/app/broker.js
+	(c) 2013 Sam Caldwell.  All Rights Reserved.
+	
+	This is the broker web service.  The broker acts as the
+	front-end web service for Nemesis API and NemesisFS Agent
+	access by user application servers.  On the back-end, the 
+	broker acts as the point where--
+	
+		*policy is stored/enforced
+		*objects are encrypted and decrypted.
+*/
 var fs = require('fs');
 var file = '/srv/nemesis/etc/nemesis/app/broker.config.json';
  
@@ -7,8 +18,6 @@ fs.readFile(file, 'utf8', function (err, data) {
     console.log('Error: ' + err);
     return;
   }
- 
-  data = JSON.parse(data);
- 
+  
   console.log(data);
 });
