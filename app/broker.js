87dc7572386808dbaca5dab51b11e9fde04d60bb
x684867 <mail@samcaldwell.net>
2013-10-06 21:25:16 -0500

    Trial code for the node.js application

--- app/broker.js
+++ app/broker.js
@@ -20,31 +20,30 @@ config_file=process.argv[2];
 console.log("    config_file: "+config_file);
 
 var fs=require('fs');
-console.log('reading config file');
-fs.readFile(config_file,function(err,data){
-	console.log('reading...');
+fs.readFile(config_file,'utf8',function(err,data){
 	if(err){
 		console.log("Error reading configuration file.");
-		console.log("  config_file:"+config_file);
-		console.log("        error:"+err);
 		throw new Exception();
 	}else{
-		/*Read the configuration file.*/
-		config=JSON.parse(data);	
-		/*Iterate through the configuration and spawn enough workers.*/
-		if(config.workers==undefined){
-			console.log("No workers defined in config_file");
-			console.log("check config_file ("+config_file+")");
-			console.log(" ")
-			throw new Exception();
-		}else{
-			config.workers.forEach(function(worker_data,index,array){
-				
-				/*Spawn the web worker.*/
-				console.log("worker_data:");
-				console.log("   "+worker);
-				
-			});
+		config=JSON.parse(data);
+		if(config==undefined){
+			console.log("config is undefined or invalid JSON");
+		}else{	
+			if(config.workers==undefined){
+				console.log("No workers defined in config_file");
+				console.log("check config_file ("+config_file+")");
+				console.log(" ")
+				throw new Exception();
+			}else{
+				config.workers.forEach(function(worker_data,index,array){
+					
+					/*Spawn the web worker.*/
+					console.log("worker_data:");
+					console.log("   "+worker);
+					
+			
+				});
+			}
 		}
 	}
 });
