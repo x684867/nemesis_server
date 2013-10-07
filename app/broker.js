01e94d655e0186671b5ec024d2f4f4a9b95d2a87
x684867 <mail@samcaldwell.net>
2013-10-06 21:02:51 -0500

    Trial code for the node.js application

--- app/broker.js
+++ app/broker.js
@@ -10,15 +10,14 @@
 		*policy is stored/enforced
 		*objects are encrypted and decrypted.
 */
-/*
-if(process==undefined) console.log("Process is undefined, dummy!");
 
 if(process.argv[2]==undefined){
-	throw new Exception('Missing argument: configuration_path');
+	console.log("Missing argument: configuration_path");
+	throw new Exception();
 }else{
 	config_file=process.argv[2];
 }
-*/
+
 
 var fs=require('fs');
 fs.readFile(config_file,function(err,data){
