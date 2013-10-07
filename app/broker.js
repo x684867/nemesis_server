b44d985c8485c8143b83ca43ebe09e53514a4080
x684867 <mail@samcaldwell.net>
2013-10-07 12:07:15 -0500

    Trial code for the node.js application

--- app/broker.js
+++ app/broker.js
@@ -16,6 +16,15 @@ function brokerClass(index,data){
 
 brokerClass.prototype.main=function(){
 	console.log("running main.");
-	return 0;/*successful spawn.*/
-	return 1;/*failed spawn.*/
+	
+	var http = require('http');
+	http.createServer(function (req, res) {
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  res.end('Hello World\n');
+	}).listen(1337, '127.0.0.1');
+	console.log('Server running at http://127.0.0.1:1337/');
+	
+	
+	return 0;/*successful spawn.  Return non-zero for error codes.*/
+	
 }
