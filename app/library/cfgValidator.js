210f201669ea42fb4922952e47cf456fe14d5355
x684867 <mail@samcaldwell.net>
2013-10-09 03:42:11 -0500

    Trial code for the node.js application

--- app/library/cfgValidator.js
+++ app/library/cfgValidator.js
@@ -29,25 +29,19 @@
 	
 */
 
-module.exports=validateConfig;
+module.exports=v;
 
-function validateConfig(c){
-	if(isValidServerConfig(c)){
-		return c;
-	}else{
-		throw new Error('Invalid or malformed JSON configuration.');
-	}
-}
+function s(x){return (typeof(x)=='string')?true:false;}
+function n(n){return (typeof(x)=='number')?true:false;}
+function o(x){return (typeof(x)=='object')?true:false;}
+function a(x){return (typeof(x.every)=='function')?true:false;}	
+function i(x,j,k){return this.n(x.workerId)&&this.s(x.ipAddress)&&this.n(x.ipPort)
+function e(x){return x.every(i);}
 
-function isValidServerConfig(c){
-	function s(x){return (typeof(x)=='string')?true:false;}
-	function n(n){return (typeof(x)=='number')?true:false;}
-	function o(x){return (typeof(x)=='object')?true:false;}
-	function a(x){return (typeof(x.every)=='function')?true:false;}	
-	function i(x,j,k){return this.n(x.workerId)&&this.s(x.ipAddress)&&this.n(x.ipPort)
-	function e(x){return x.every(i);}
-	return 	o(c)&&s(c.serverType)&&o(c.monitor)&&o(c.monitor.heartbeat)&&
-			n(c.monitor.heartbeat.interval)&& n(c.monitor.heartbeat.threshold)&&
-			o(c.monitor.statistics)&&n(c.monitor.statistics.interval)&&a(c.workers)&&
-			e(c.workers);
+function v(c){
+	if(o(c)&&s(c.serverType)&&o(c.monitor)&&o(c.monitor.heartbeat)&&
+	   n(c.monitor.heartbeat.interval)&& n(c.monitor.heartbeat.threshold)&&
+	   o(c.monitor.statistics)&&n(c.monitor.statistics.interval)&&a(c.workers)&&
+	   e(c.workers)) return c;
+	throw new Error('Invalid or malformed JSON configuration.');
 }
