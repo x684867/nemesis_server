1f512b1d478250aeb04e9bcc07045121057bfbda
x684867 <mail@samcaldwell.net>
2013-10-10 15:28:13 -0500

    Added fixes to config.js

--- app/library/config.js
+++ app/library/config.js
@@ -27,45 +27,42 @@
 */
 module.exports=config;
 
+const TOBJ='object';
+const TSTR='string';
+const TNUM='number';
+const TFUNC='function';
+const TBOOL='boolean';
+	
 function config(filename){
 	var fs =require('fs');
 	var logger=require('/srv/nemesis/app/logger/logger.js');
 	var log=new logger("config.js(main)");	
-	
-	const TOBJ='object';
-	const TSTR='string';
-	const TNUM='number';
-	const TFUNC='function';
-	const TBOOL='boolean';
-
 	log.drawBanner("starting config constructor");
 
 	this.data={status:undefined};
 	
-	var E_CODES=[
-		/*00*/'Invalid configuration object',
-		/*01*/'Invalid monitor configuration object',
-		/*02*/'Invalid heartbeat monitor configuration object',
-		/*03*/'Invalid stats monitor configuration object',
-		/*04*/'Invalid workers configuration object',
-		/*05*/'Invalid serverType in configuration object',
-		/*06*/'Invalid heartbeat interval in configuration object',
-		/*07*/'Invalid heartbeat interval in configuration object',
-		/*08*/'Invalid stats interval in configuration object',
-		/*09*/'Invalid worker collection object (array)',
-		/*10*/'Invalid workerId in worker configuration object',
-		/*11*/'Invalid IP address string in worker configuration object',
-		/*12*/'Invalid network port (number) in worker configuration object',
-		/*13*/'Invalid worker collection object (not an array)',
-		/*14*/'Invalid worker ssl parameter (expect boolean)',
-		/*15*/'Invalid ssl object',
-		/*16*/'Invalid ssl private_key (expect string)',
-		/*17*/'Invalid ssl public_key (expect string)',
-		/*18*/'Invalid ssl ca_cert (expect string)',
-		/*19*/'Missing file (private_key)',
-		/*20*/'Missing file (public_key)',
-		/*21*/'Missing file (ca_cert)'
-	];
+	const E_BAD_OBJ='Invalid configuration object';
+	const E_BAD_MONITOR_OBJ='Invalid monitor configuration object';
+	const E_BAD_HB_OBJ='Invalid heartbeat monitor configuration object';
+	const E_BAD_STAT_OBJ='Invalid stats monitor configuration object';
+	const E_BAD_WRKR_COLL_OBJ='Invalid workers configuration object';
+	const E_BAD_SRVR_TYPE='Invalid serverType (expect string)';
+	const E_BAD_HB_INTERVAL='Invalid heartbeat interval (expect number)';
+	const E_BAD_HB_THRESHOLD='Invalid heartbeat interval in configuration object';
+	const E_BAD_STAT_INTERVAL='Invalid stats interval in configuration object';
+	const E_BAD_WRKR_OBJ='Invalid worker collection object (array)';
+	const E_BAD_WRKR_ID='Invalid workerId in worker configuration object';
+	const E_BAD_WRKR_IP='Invalid IP address string in worker configuration object';
+	const E_BAD_WRKR_PORT='Invalid network port (number) in worker configuration object';
+	const E_BAD_WRKR_ARR='Invalid worker collection object (not an array)',
+	const E_BAD_WRKR_SSL='Invalid worker ssl parameter (expect boolean)';
+	const E_BAD_SSL_OBJ='Invalid ssl object';
+	const E_BAD_SSL_KEY='Invalid ssl private_key (expect string)';
+	const E_BAD_SSL_CRT='Invalid ssl public_key (expect string)';
+	const E_BAD_SSL_CA='Invalid ssl ca_cert (expect string)';
+	const E_MISSING_SSL_KEY='Missing file (private_key)';
+	const E_MISSING_SSL_CRT='Missing file (public_key)';
+	const E_MISSING_SSL_CA'Missing file (ca_cert)';
 	
 	if(fs.lstatSync(filename).isFile()){
 		try{
@@ -80,28 +77,26 @@ function config(filename){
 	
 	var requireSSL=false;
 	
-	if(typeof(this.data)!=TOBJ) throw new Error(E_CODES[0]);
-		
+	if(typeof(this.data)!=TOBJ) throw new Error(E_BAD_OBJ);
 	/*Verify root properties.*/
-	if(typeof(this.data.serverType)!=TSTR) throw new Error(E_CODES[5]);
-	
+	if(typeof(this.data.serverType)!=TSTR) throw new Error(E_BAD_SRVR_TYPE);
 	/*Verify the monitor configuration.*/
-	if(typeof(this.data.monitor)!=TOBJ) throw new Error(E_CODES[1]);
-	if(typeof(this.data.monitor.heartbeat)!=TOBJ) throw new Error(E_CODES[2]);
-	if(typeof(this.data.monitor.heartbeat.interval)!=TNUM) throw new Error(E_CODES[6]);
-	if(typeof(this.data.monitor.heartbeat.threshold)!=TNUM) throw new Error(E_CODES[7]);
-	if(typeof(this.data.monitor.statistics)!=TOBJ) throw new Error(E_CODES[3]);
-	if(typeof(this.data.monitor.statistics.interval)!=TNUM) throw new Error(E_CODES[8]);
+	if(typeof(this.data.monitor)!=TOBJ) throw new Error(E_BAD_MONITOR_OBJ);
+	if(typeof(this.data.monitor.heartbeat)!=TOBJ) throw new Error(E_BAD_HB_OBJ);
+	if(typeof(this.data.monitor.heartbeat.interval)!=TNUM) throw new Error(E_BAD_HB_INTERVAL);
+	if(typeof(this.data.monitor.heartbeat.threshold)!=TNUM) throw new Error(E_BAD_HB_THRESHOLD);
+	if(typeof(this.data.monitor.statistics)!=TOBJ) throw new Error(E_BAD_STAT_OBJ);
+	if(typeof(this.data.monitor.statistics.interval)!=TNUM) throw new Error(E_BAD_STAT_INTERVAL);
 
 	/*Worker Verification*/
-	if(typeof(this.data.workers)!=TOBJ) throw new Error(E_CODES[4]);
-	if(typeof(this.data.workers.forEach)!=TFUNC) throw new Error(E_CODES[13]);
+	if(typeof(this.data.workers)!=TOBJ) throw new Error(E_BAD_WRKR_COLL_OBJ);
+	if(typeof(this.data.workers.forEach)!=TFUNC) throw new Error(E_BAD_WRKR_ARR);
 	this.data.workers.forEach(function(w,i,a){
-		if(typeof(w)!=TOBJ) throw new Error(E_CODES[9]);
-		if(typeof(w.workerId)!=TNUM) throw new Error(E_CODES[10]);
-		if(typeof(w.ipAddress)!=TSTR) throw new Error(E_CODES[11]);
-		if(typeof(w.ipPort)!=TNUM) throw new Error(E_CODES[12]);
-		if(typeof(w.ssl)!=TBOOL) throw new Error(E_CODES[14]);
+		if(typeof(w)!=TOBJ) throw new Error(E_BAD_WRKR_OBJ);
+		if(typeof(w.workerId)!=TNUM) throw new Error(E_BAD_WRKR_ID);
+		if(typeof(w.ipAddress)!=TSTR) throw new Error(E_BAD_WRKR_IP);
+		if(typeof(w.ipPort)!=TNUM) throw new Error(E_BAD_WRKR_PORT);
+		if(typeof(w.ssl)!=TBOOL) throw new Error(E_BAD_WRKR_SSL);
 		if(w.ssl){
 			w.ipPort=443
 			log.write("NOTICE: ssl=true.  Override ipPort to 443");
@@ -111,18 +106,18 @@ function config(filename){
 	if(this.data.requireSSL){
 		/*SSL Verification must come AFTER the worker verification*/
 		log.write("SSL Verification required.");
-		if(typeof(this.data.ssl)!=TOBJ) throw new Error(E_CODES[15]);
-		if(typeof(this.data.ssl.private_key)!=TSTR) throw new Error(E_CODES[16]);
-		if(typeof(this.data.ssl.public_key)!=TSTR) throw new Error(E_CODES[17]);
-		if(typeof(this.data.ssl.ca_cert)!=TSTR) throw new Error(E_CODES[18]);	
+		if(typeof(this.data.ssl)!=TOBJ) throw new Error(E_BAD_SSL_OBJ);
+		if(typeof(this.data.ssl.private_key)!=TSTR) throw new Error(E_BAD_SSL_KEY);
+		if(typeof(this.data.ssl.public_key)!=TSTR) throw new Error(E_BAD_SSL_CRT);
+		if(typeof(this.data.ssl.ca_cert)!=TSTR) throw new Error(E_BAD_SSL_CA);	
 
 		log.write("Verifying that SSL Certificate/Key files exist.");
-		[	this.data.ssl.private_key,
-			this.data.ssl.public_key,
-			this.data.ssl.ca_cert
-		].forEach(function(f,i,a){
-			if(!fs.lstatSync(f).isFile()) throw new Error(E_CODES[i+19]);
-			log.write("  EXISTS!  ["+i+"]:["+f+"]");
+		[	{"file":this.data.ssl.private_key,"error":E_MISSING_SSL_KEY},
+			{"file":this.data.ssl.public_key,"error":E_MISSING_SSL_CERT},
+			{"file":this.data.ssl.ca_cert,"error":E_MISSING_SSL_CA}
+		].forEach(function(o,i){
+			if(!fs.lstatSync(o.file).isFile()) throw new Error(o.error);
+			log.write("  EXISTS!  ["+i+"]:["+o.file+"]");
 		});
 	}else{
 		log.write("SSL Verification not required.");
