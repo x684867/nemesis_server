9d546ff32e864bda7f9b26f09ddd346fc23b444b
x684867 <mail@samcaldwell.net>
2013-10-10 01:13:19 -0500

    Trial code for the node.js application

--- app/worker.js
+++ app/worker.js
@@ -40,7 +40,7 @@ process.on('message', function(msg){
 				log.drawLine();
 				log.write('Loading the server specified in the config.');
 				
-				serverFactory=require('./app/servers/'+msg.data.type+'.js');
+				serverFactory=require('/srv/nemesis/app/servers/'+msg.data.type+'.js');
 				log.write('Instantiating the server.');
 				server=new serverFactory(msg.data.id,msg.data.config);
 				log.write('Server is instantiated.');
