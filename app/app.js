2d106d085f6f4ce51b52a9455b9e80b95349d918
x684867 <mail@samcaldwell.net>
2013-10-10 17:38:54 -0500

    Some housekeeping and a bug fix for app.js termination after init.

--- app/app.js
+++ app/app.js
@@ -30,7 +30,7 @@
 			*Begin service development.
 	---------------------------------------------------------------------------------
 */
-global const LOGGER_CLASS='/srv/nemesis/app/logger/logger.js';
+global LOGGER_CLASS='/srv/nemesis/app/logger/logger.js';
 
 const CHILD_PROCESS_WRAPPER='/srv/nemesis/app/library/worker.js';
 const PID_WRITER_SCRIPT='/srv/nemesis/app/library/pidWriter.js';
