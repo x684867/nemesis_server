547e0f01031977c848670640105108846b86e3bd
x684867 <mail@samcaldwell.net>
2013-10-11 00:26:16 -0500

    Adding logger configuration to keep the logger isolated.

--- app/logger/logger.js
+++ app/logger/logger.js
@@ -3,20 +3,98 @@
 	Nemesis Logger
 	(c) 2013 Sam Caldwell.  All Rights Reserved.
 	
-	This file creates the Nemesis Logger, which will allow the application
-	to write output to stdout or syslog with only a single change in this 
-	file.
+	This file creates a syslog logging tool.
 	
-	While working on development, this file will simply write output to
-	stdout.  It will take care of any formatting or other cosmetic issues.
-	But before this project goes into production, syslog will be added so that
-	daemonized processes can send their output to syslog without much effort.
+	Currently only TCP connections are supported.  This will go production with 
+	encrypted communications over TLS (TCP).
 */
+
 module.exports=logger;
 
-function logger(logSource,enableDebug){
-	if(typeof(enableDebug)!='boolean') enableDebug=false;
+const TSTR='string';
+const TNUM='number';
 
+const SYSLOG_CONFIG='/srv/nemesis/app/logger/logger.config.json';
+const SYSLOG_PARAMETERS='/srv/nemesis/app/logger/parameters.json';
+
+function logger(source,priority,facility){
+	if(typeof(source)!=TSTR) throw new Error('SYSLOG source not a string (expected)');
+	if(typeof(priority)!=TNUM) throw new Error('SYSLOG priority not a number (expected)');
+	if(typeof(facility)!=TNUM) throw new Error('SYSLOG facility not a number (expected)');
+	var config=(function(){
+		try{
+			return JSON.parse((fs=require('fs')).readFileSync(SYSLOG_CONFIG));
+		}catch(e){
+			throw new Error('SYSLOG config file failed to read.');
+		}
+	});
+	if(typeof(config)!='object') throw new Error('SYSLOG config file failed to load');
+	var syslog=(function(){
+		try{
+			return JSON.parse((fs=require('fs')).readFileSync(SYSLOG_PARAMETERS));
+		}catch(e){
+			throw new Error('SYSLOG parameters file failed to read.');
+		}
+	});
+	if(typeof(config)!='object') throw new Error('SYSLOG parameters file failed to load');
+
+	this.send=function(message){
+	
+		var prefix=config.facility+config.severity+source;
+		var p1=0;
+		var p2=0;
+		var mL=message.length;
+		var mS=config.msgSize - prefix.length -1;
+		
+		if(mS<64) throw new Error('Message Size cannot be less than 64.');
+		
+		while(p1<mL){
+		
+			if( ( p2=p1+mS ) > mL ) p2=mL;
+			
+			msgSlice=prefix + ":" + message.substring(p1,p2);
+			
+			var client=(require('net')).connect({port:config.port},function(){
+				/*parse the message into msgSize chunks and write them to SYSLOG*/
+			
+				client.write(msgSlice);
+		
+			});
+		}
+		client.end();
+	}
+		
+
+function logger(logSource,config){
+	if(typeof(config)!='object') throw new Error("expected config to be type object");
+	
+	var connection=(require('dgram')).createSocket(netProto);
+	
+	this.send=function(message){
+		var buffer=Array(message.length/config.msgSize+2);
+		
+		/*Slice the message into chunks if length exceeds msgSize.*/
+		
+		var prefix=
+		var p1=0;
+		var p2;
+		var ml=message.length;
+		var ms=config.msgSize - prefix.length -1;
+		
+		if(ms<64) throw new Error('Message Size cannot be less than 64.');
+		
+		while(p1<=ml){
+
+			if( ( p2=p1+ms ) > ml ) p2=ml;
+
+			slice=message.substring(p1,p2);
+			
+			connection.send(slice,0,slice.length,config.port,config.ip);
+			
+			p1=p2+1;
+		
+	}
+	
 	this.source=(logSource==undefined)?'NoSource':logSource;
 	
 	this.indent=function(i){return Array(i).join(" ");}
@@ -47,3 +125,6 @@ function logger(logSource,enableDebug){
 		this.drawLine(60,i);
 	}
 }
+
+
+
