191f74133eaa1ca82578df50f4c58d59dce8f089
x684867 <mail@samcaldwell.net>
2013-10-10 20:33:57 -0500

    increased granularity for the app.js logging

--- app/app.js
+++ app/app.js
@@ -68,7 +68,7 @@ pidFile=new (require(PID_WRITER_SCRIPT))(config.data.pidDirectory);
 config.data.workers.forEach(
 	function(workerConfig,id,array){
 		if(workerConfig.enabled){
-			log.source="app.js(config.data.workers.forEach)";
+			log.source="app.js(cdw loop)";
 			log.drawLine(60);
 			log.write("fork process with wrapper");
 			var processFactory=require('child_process');
