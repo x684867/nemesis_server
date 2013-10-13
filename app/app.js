b0ec9f130c38a64c77da171b954c73c6553a1754
x684867 <mail@samcaldwell.net>
2013-10-12 22:32:41 -0500

    Fix count issue in console output.

--- app/app.js
+++ app/app.js
@@ -111,10 +111,16 @@ var app={
 								case 11:
 									delay=(new Date()).getTime()/1000 - msg.data;
 									if(delay < config.data.monitor.heartbeat.threshold){
-										console.log(timestamp()+"{P:11}beat w#"+id+":good");
+										console.log(
+													 timestamp()
+													+"{P:11}beat w#"+id+":good"
+										);
 										this.pollMonitoring(msg);
 									}else{
-										console.log(timestamp()+"{P:11}beat w#"+id+":slow");
+										console.log(
+													 timestamp()
+													+"{P:11}beat w#"+id+":slow"
+										);
 										this.pollStatistics(msg);
 									}
 									break;
@@ -122,36 +128,57 @@ var app={
 								case 97:console.log(timestamp()+"{P:97}not impl.");break;
 								case 99:console.log(timestamp()+"{P:99}not impl.");break;
 								default:
-									throw new Error(timestamp()+":Unk/Inv code:"+msg.code);
+									throw new Error(
+													timestamp()+":Unk/Inv code:"+msg.code
+									);
 									break;
 							}
 		  				});
 		  				console.log(timestamp()+"++setup error listener");
 						child.on('error',function(m){
-							if(!validator.isValidError(m)) throw new Error(E_INV_MSG_ON_ERROR_EVENT);
-							throw new Error(E_FEATURE_NOT_IMPLEMENTED+":worker.on("+m+")");
+							if(!validator.isValidError(m)){ 
+								throw new Error(E_INV_MSG_ON_ERROR_EVENT);
+							}
+							throw new Error(
+												E_FEATURE_NOT_IMPLEMENTED
+												+":worker.on("+m+")"
+							);
 						});
 						console.log(timestamp()+"++setup exit listener");
 						child.on('exit',function(code,signal){
-							console.log(timestamp()+"worker exit ("+id+")("+code+","+signal+") count:"+global.procs.length);
+							console.log(
+								 timestamp()
+								+"worker exit ("+id+")("+code+","+signal+") "
+								+"count:"+global.procs.length
+							);
 						});
 						console.log(timestamp()+"++setup close listener");
 						child.on('close',function(code,signal){
-							console.log(timestamp()+"worker close ("+id+")("+code+","+signal+") count:"+global.procs.length);
+							console.log(
+								 timestamp()
+								+"worker close ("+id+")("+code+","+signal+") "
+								+"count:"+global.procs.length
+							);
 							global.pidCount--;
 						});
 						console.log(timestamp()+"++setup disconnect listener");
 						child.on('disconnect',function(){
-							console.log(timestamp()+"worker disconnect ("+id+") Cnt:"+global.procs.length);
+							console.log(
+										 timestamp()+"worker disconnect ("+id+") "
+										+"Cnt:"+global.procs.length
+							);
 							global.pidCount
 						});
-						console.log(timestamp()+"end of worker init. Cnt:"+global.procs.length);
+						console.log(
+									 timestamp()
+									+"end of worker init. Cnt:"+global.procs.length
+						);
 					}else{
 		  				console.log(timestamp()+"child process failed to spawn.");
 		  			}
 				}else{
 					console.log(
-						timestamp()+"worker #"+id+"/"+workerList.length+" disabled."
+						timestamp()+"worker #"+id+"/"+(workerList.length-1)+" disabled."
 					);
 				}
 				log.list_pids();
