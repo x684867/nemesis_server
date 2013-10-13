81c0144b1a7e29f097c0a8b9ad6dc9b4522c8136
x684867 <mail@samcaldwell.net>
2013-10-12 23:04:29 -0500

    Refactored msgValidator.js to fix issues found in worker.js

--- app/library/msgValidator.js
+++ app/library/msgValidator.js
@@ -2,7 +2,10 @@
 	General Message Validator
 	/srv/nemesis/app/library/msgValidator.js 
 	(c) 2013 Sam Caldwell.  All Rights Reserved.  
+
+
  */
+module.exports=validator
 const LOGGER_SOURCE='lib.msgValidator';
 const LOGGER_CLASS='/srv/nemesis/app/logger/logger.js';
 /* */
@@ -38,62 +41,64 @@ const MSG_CD2_D_CORRECT='{code:2,data:<object>} is correctly formatted.';
 function typeCheck(d,t,e){if(typeof(d)!=t) throw new Error(e);}
 function isUndefined(d,e){if(typeof(d)=='undefined') throw new Error(e);}
 /* */
-function isValidError(m){
-	return (typeof(m)==TOBJ)?true:false;
-}
-/* */
-function isValidMsg(m){
-	typeCheck(m,TOBJ,E_M_NOT_OBJ);
-	isUndefined(m.code,E_M_CD_NOT_SET);
-	typeCheck(m.code,TNUM,E_M_CD_NOT_NUM);
-	switch(m.code){
-		case 0:return true;break;
-		case 1:return true;break;
-		case 2:
-			typeCheck(m.data,TOBJ,E_M_CD2_NON_OBJ);
-			typeCheck(m.data,TOBJ,E_M_CD2_NON_OBJ);
-			isUndefined(m.data.id,E_M_CD2_D_ID_UNDEF);
-			isUndefined(m.data.type,E_M_CD2_D_TYPE_UNDEF);
-			isUndefined(m.data.config,E_M_CD2_D_CFG_UNDEF);
-			typeCheck(m.data.id,TNUM,E_M_CD2_D_ID_NAN);
-			typeCheck(m.data.type,TSTR,E_M_CD2_D_TYPE_NSTR);
-			typeCheck(m.data.config,TOBJ,E_M_CD2_CFG_NAO);
-			isUndefined(m.data.config.workerId,E_M_CD2_CFG_WID_UNDEF);
-			isUndefined(m.data.config.ipAddress,E_M_CD2_CFG_IP_UNDEF);
-			isUndefined(m.data.config.ipPort,E_M_CD2_CFG_PORT_UNDEF);
-			typeCheck(m.data.config.workerId,TNUM,E_M_CD2_CFG_ID_NAN);
-			typeCheck(m.data.config.ipAddress,TSTR,E_M_CD2_CFG_IP_NSTR);
-			typeCheck(m.data.config.ipPort,TNUM,E_M_CD2_CFG_PORT_NAN);
-			log.write(MSG_CD2_D_CORRECT);
-			return true;
-			break;		
-		case 3:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
-		case 4:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
-		case 5:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
-		case 6:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
-		case 7:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
-		case 8:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
-		case 9:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
-		case 10:return true;break;
-		case 11:
-			isUndefined(m.data,E_M_CD11_D_UNDEF);
-			typeCheck(m.data,TNUM,E_M_CD11_D_NON_OBJ);
-			return true;
-			break;					
-		case 12:return true;break;
-		case 13:
-			isUndefined(m.data,E_M_CD13_D_UNDEF);
-			typeCheck(m.data,TOBJ,E_M_CD13_D_NOT_OBJ);
-			return true;
-			break;
-		case 95:return true;break;
-		case 96:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
-		case 97:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
-		case 98:return true;break;
-		case 99:return true;break;
-		default:
-			throw new Error(E_M_UNKNOWN_CODE);
-			return false;
-			break;
-	}/*end of switch()*/
+
+validator={
+	isValidError:function(m){
+		return (typeof(m)==TOBJ)?true:false;
+	}
+	isValidMsg:function(m){
+		typeCheck(m,TOBJ,E_M_NOT_OBJ);
+		isUndefined(m.code,E_M_CD_NOT_SET);
+		typeCheck(m.code,TNUM,E_M_CD_NOT_NUM);
+		switch(m.code){
+			case 0:return true;break;
+			case 1:return true;break;
+			case 2:
+				typeCheck(m.data,TOBJ,E_M_CD2_NON_OBJ);
+				typeCheck(m.data,TOBJ,E_M_CD2_NON_OBJ);
+				isUndefined(m.data.id,E_M_CD2_D_ID_UNDEF);
+				isUndefined(m.data.type,E_M_CD2_D_TYPE_UNDEF);
+				isUndefined(m.data.config,E_M_CD2_D_CFG_UNDEF);
+				typeCheck(m.data.id,TNUM,E_M_CD2_D_ID_NAN);
+				typeCheck(m.data.type,TSTR,E_M_CD2_D_TYPE_NSTR);
+				typeCheck(m.data.config,TOBJ,E_M_CD2_CFG_NAO);
+				isUndefined(m.data.config.workerId,E_M_CD2_CFG_WID_UNDEF);
+				isUndefined(m.data.config.ipAddress,E_M_CD2_CFG_IP_UNDEF);
+				isUndefined(m.data.config.ipPort,E_M_CD2_CFG_PORT_UNDEF);
+				typeCheck(m.data.config.workerId,TNUM,E_M_CD2_CFG_ID_NAN);
+				typeCheck(m.data.config.ipAddress,TSTR,E_M_CD2_CFG_IP_NSTR);
+				typeCheck(m.data.config.ipPort,TNUM,E_M_CD2_CFG_PORT_NAN);
+				log.write(MSG_CD2_D_CORRECT);
+				return true;
+				break;		
+			case 3:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
+			case 4:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
+			case 5:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
+			case 6:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
+			case 7:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
+			case 8:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
+			case 9:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
+			case 10:return true;break;
+			case 11:
+				isUndefined(m.data,E_M_CD11_D_UNDEF);
+				typeCheck(m.data,TNUM,E_M_CD11_D_NON_OBJ);
+				return true;
+				break;					
+			case 12:return true;break;
+			case 13:
+				isUndefined(m.data,E_M_CD13_D_UNDEF);
+				typeCheck(m.data,TOBJ,E_M_CD13_D_NOT_OBJ);
+				return true;
+				break;
+			case 95:return true;break;
+			case 96:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
+			case 97:throw new Error(E_M_NOT_IMPLEMENTED+":msg="+m);return false;break;
+			case 98:return true;break;
+			case 99:return true;break;
+			default:
+				throw new Error(E_M_UNKNOWN_CODE);
+				return false;
+				break;
+		}/*end of switch()*/
+	}
 }
