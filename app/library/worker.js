33287261d97b76ce4cc357ebdfc84fe69622e63e
x684867 <mail@samcaldwell.net>
2013-10-12 23:17:33 -0500

    Working to fix problems with the validator in worker.js and app.js, etc.

--- app/library/worker.js
+++ app/library/worker.js
@@ -44,8 +44,8 @@ function workerClass(){
 	process.on('close',function(code){log.write("worker close");});
 	process.on('message', function(msg){
 		log.write(LOG_MSG_RECD);
-		var validator=require(VALIDATOR_CLASS);
-		if(!((new validator).isValidMsg(msg)))throw(E_INV_MSG_CHILD);
+		validator=require(VALIDATOR_CLASS);
+		if(!((validator).isValidMsg(msg)))throw(E_INV_MSG_CHILD);
 		switch(msg.code){
 				
 			case 0:/*Parent (code:0) => Child (code:1) => Parent*/
