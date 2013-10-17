1dd6a074cdc3cbfdcdbc7e556020498739c148c6
x684867 <mail@samcaldwell.net>
2013-10-17 15:40:09 -0500

    looking for the bug in the config file.  did some housekeeping to  make the file more readable.

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -23,7 +23,7 @@
 */
 root.title="Nemesis"
 root.version="2013.10.17.12.52"; /*Update when pushing to master branch.*/
-root.conf_dir='/srv/nemesis/etc/nemesis';
+root.conf_dir='/srv/nemesis/app/config';
 /*
 	Load the main configuration file.
 */
