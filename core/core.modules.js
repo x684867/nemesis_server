ffaaf2fd2ef59f617c1480bf259235aa027be949
x684867 <mail@samcaldwell.net>
2013-10-17 15:54:53 -0500

    Fixed error (typo) in core.modules.js

--- core/core.modules.js
+++ core/core.modules.js
@@ -6,33 +6,31 @@
 		root.config.modules (defined by bootstrap.js).
  */
  
-module.exports=function(){
+module.exports=load_modules;
 
-	this.load_modules=function(){
+function load_modules(){
 		var o={}
-
-			root.config.modules.forEach(
-			function(m,i,a){
-				if(typeof(m)=='object'){
+		root.config.modules.forEach(
+		function(m,i,a){
+			if(typeof(m)=='object'){
+			
+				init_object=function(o){ if(typeof(o)==undefined) o={} };
 				
-					init_object=function(o){ if(typeof(o)==undefined) o={} };
-					
-					init_object(root.config.modules);
-					init_object(root.config.modules[m.group]);
-					init_object(root.config.modules[m.group][m.name]);
-					
-					root.config.modules[m.group][m.name]=require(m.file);
-					
-				}else{
-					console.log("In "+module.filename+" root.errors is not yet defined.");
-					throw new Error(root.error.messages.bootstrap.invalidModule.text);
-				}
+				init_object(root.config.modules);
+				init_object(root.config.modules[m.group]);
+				init_object(root.config.modules[m.group][m.name]);
+				
+				root.config.modules[m.group][m.name]=require(m.file);
+				
+			}else{
+				console.log("In "+module.filename+" root.errors is not yet defined.");
+				throw new Error(root.error.messages.bootstrap.invalidModule.text);
 			}
-		);
-		if(root.debug){
-			console.log("-----MODULES LOADED-----");
-			console.dir(root.config.modules);
-			console.log("------------------------");
 		}
+	);
+	if(root.debug){
+		console.log("-----MODULES LOADED-----");
+		console.dir(root.config.modules);
+		console.log("------------------------");
 	}
 }
