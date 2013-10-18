5af9eadc95c33001eae37fbf1340a4af22acca79
x684867 <mail@samcaldwell.net>
2013-10-18 08:33:37 -0500

    Added a logProcess() method for writing current process state to a given local log object.

--- core/core.process.js
+++ core/core.process.js
@@ -6,9 +6,17 @@
 module.exports=process_manager;
 
 function process_manager(){
-	var pool=Array();
+
+	process.on(
+		'uncaughtException',
+		function(err){root.app.log.error(root.error.unknown,err);}
+	);
+
+	var log=new root.modules.core.logger(module.id);
 	
-	this.list=function(){
+	root.process.pool=Array();
+	
+	root.process.pidlist=function(){
 		var p='';
 		for(i=0;i<root.app.process.pool.length;i++){
 			p=p+root.app.process.pool[i].pid+',';
@@ -16,24 +24,28 @@ function process_manager(){
 		return "["+p.substring(0,p.length-1)+"]";
 	},
 
-	this.count=function(){return root.app.process.pool.length;},
+	root.process.count=function(){return root.app.process.pool.length;},
 
-	this.init=function(){
-		process.on(
-			'uncaughtException',
-			function(err){root.app.log.error(root.error.unknown,err);}
-		);
-	},
-
-	this.fork=function(wrapper){
+	root.process.fork=function(wrapper){
 		var child=void(0);
 		try{child=root.modules.child_process.fork(wrapper);}catch(e){throw e;}
 		root.app.process.showStatus(parent.pid,child.pid,root.app.process.count());
 		return child;
 	},
 
-	this.showStatus=function(p,c,n){
+	root.process.showStatus=function(p,c,n){
 		root.log.write("{parent:"+p+",child:"+c+",count:"+n+"}");
+	},
+	
+	root.process.logProcess=function(localLog,id,process.pid,child.pid){
+		log.write(
+			'{'
+				+'"id":'+id','
+			 	+'"parentPID":'+process.pid+','
+			 	+'"childPID":'+child.pid','
+			 	+'"count":'+root.process.pool.length
+		   +'}'
+		);
 	}
 	
 }/*end of root.app.process.*/
