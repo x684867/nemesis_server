831403c62ab8bf6ee91246f8ad59dcd6ea20e976
x684867 <mail@samcaldwell.net>
2013-10-17 23:31:35 -0500

    Refactoring root.modules.process (class) to fix load issues.

--- core/core.process.js
+++ core/core.process.js
@@ -3,14 +3,12 @@
 	/srv/nemesis/app/app.process.js
 	(c) 2013 Sam Caldwell.  All Rights Reserved.
  */
-module.exports=process_manager
+module.exports=process_manager;
 
-function process_manager(){/*do nothing*/};
-
-root.process={
-	pool:Array(),
-
-	list:function(){
+function process_manager(){
+	var pool=Array();
+	
+	this.list=function(){
 		var p='';
 		for(i=0;i<root.app.process.pool.length;i++){
 			p=p+root.app.process.pool[i].pid+',';
@@ -18,22 +16,24 @@ root.process={
 		return "["+p.substring(0,p.length-1)+"]";
 	},
 
-	count:function(){return root.app.process.pool.length;},
+	this.count=function(){return root.app.process.pool.length;},
 
-	init:function(){
+	this.init=function(){
 		process.on(
 			'uncaughtException',
 			function(err){root.app.log.error(root.error.unknown,err);}
 		);
 	},
 
-	fork:function(wrapper){
+	this.fork=function(wrapper){
 		var child=void(0);
 		try{child=root.modules.child_process.fork(wrapper);}catch(e){throw e;}
 		root.app.process.showStatus(parent.pid,child.pid,root.app.process.count());
 		return child;
 	},
 
-	showStatus:function(p,c,n){root.log.write("{parent:"+p+",child:"+c+",count:"+n+"}");}
+	this.showStatus=function(p,c,n){
+		root.log.write("{parent:"+p+",child:"+c+",count:"+n+"}");
+	}
 	
 }/*end of root.app.process.*/
