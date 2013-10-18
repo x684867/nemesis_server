6d2387822700bf8f15a745597197dddce3036d0a
x684867 <mail@samcaldwell.net>
2013-10-17 23:22:11 -0500

    Fixing an issue in root.app.process.

--- core/core.process.js
+++ core/core.process.js
@@ -8,7 +8,7 @@ module.exports=process_manager
 function process_manager(){/*do nothing*/};
 
 root.app.process={
-	pool:Array();
+	pool:Array(),
 
 	list:function(){
 		var p='';
@@ -16,23 +16,23 @@ root.app.process={
 			p=p+root.app.process.pool[i].pid+',';
 		}
 		return "["+p.substring(0,p.length-1)+"]";
-	}
+	},
 
-	count:function(){return root.app.process.pool.length;}
+	count:function(){return root.app.process.pool.length;},
 
 	init:function(){
 		process.on(
 			'uncaughtException',
 			function(err){root.app.log.error(root.error.unknown,err);}
 		);
-	}
+	},
 
 	fork:function(wrapper){
 		var child=void(0);
 		try{child=root.modules.child_process.fork(wrapper);}catch(e){throw e;}
 		root.app.process.showStatus(parent.pid,child.pid,root.app.process.count());
 		return child;
-	}
+	},
 
 	showStatus:function(p,c,n){root.log.write("{parent:"+p+",child:"+c+",count:"+n+"}");}
 	
