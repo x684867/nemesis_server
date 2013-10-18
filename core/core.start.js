7e14642d739633e1399f415be74e2b38c6e95d11
x684867 <mail@samcaldwell.net>
2013-10-18 12:26:03 -0500

    Refactor code.start.js to begin moving child process init work to core.process.js or other process related module.

--- core/core.start.js
+++ core/core.start.js
@@ -5,6 +5,110 @@
  */
 module.exports=app_start;
 
+function setChildProcessUncaughtExceptionEvent(c){
+	c.on('uncaughtException', function(e) {
+		root.error.raise(root.error.messages.app.start.uncaughtException,"pid:"+c.pid+",error:"+e);
+		c.send(root.ipc.message.childSuicide());
+	});
+}
+
+function setChildMessageEvent(c){
+	c.on('message',function(msg){
+		if(!(root.modules.core.messages.isValidMsg(msg))){
+			root.error.raise(root.error.messages.app.start.invalidMessageEncountered);
+		}
+		switch(msg.code){
+			case root.ipc.code.workerAlive:
+				c.send(
+					root.ipc.message.configureWorker(
+						c.pid,
+						id,
+						root.config.service.data.serverType,
+						config,
+						root.config.service.data.ssl.private_key,
+						root.config.service.data.ssl.public_key,
+						root.config.service.data.ssl.ca_cert
+					)
+				);
+				/* Update statistics. */
+				break;
+				
+			case root.ipc.code.workerOnline:
+				/* Update statistics. */
+				break;
+				
+			case root.ipc.code.workerFailed:
+				/* Update statistics. */
+				break;
+				
+			case root.ipc.code.workerPingReply:
+				delay=(new Date()).getTime()/1000 - msg.data;
+				if(delay < root.config.service.data.monitor.heartbeat.threshold){
+					log.write("root.ipc.code.workerPingReply w#"+id+":good");
+					this.pollMonitoring(msg);
+				}else{
+					log.write("root.ipc.code.workerPingReply w#"+id+":slow");
+					this.pollStatistics(msg);
+				}
+				break;
+			
+			case root.ipc.code.workerStatsReply:
+				log.write("root.ipc.code.workerStatsReply not impl.");
+				break;
+			
+			case root.ipc.code.childSuicideRequest:
+				c.kill('SIGKILL');
+				break;
+				
+			case root.ipc.code.childKillReply:
+				c.kill('SIGKILL');
+				break;
+		
+			default:
+				root.error.raise(root.error.messages.invalidMessageEncountered);
+				break;
+		}
+	});
+}
+
+function setChildErrorEvent(c){
+	c.on('error',function(m){
+		if(!validator.isValidError(m)){ throw new Error(E_INV_MSG_ON_ERROR_EVENT);}
+		throw new Error(E_FEATURE_NOT_IMPLEMENTED+":worker.on("+m+")");
+	});
+}
+function setChildExitEvent(child){
+	console.log(timestamp()+"++setup exit listener");
+	child.on('exit',function(code,signal){
+						console.log(
+							 timestamp()
+							+"worker exit ("+id+")("+code+","+signal+") "
+							+"count:"+global.procs.length
+						);
+	});
+}
+function setChildCloseEvent(child){
+	console.log(timestamp()+"++setup close listener");
+	child.on('close',function(code,signal){
+		console.log(
+			 timestamp()
+			+"worker close ("+id+")("+code+","+signal+") "
+			+"count:"+global.procs.length
+		);
+		global.pidCount--;
+	});
+}
+function setChildDisconnectEvent(child){
+	console.log(timestamp()+"++setup disconnect listener");
+	child.on('disconnect',function(){
+		console.log(
+					 timestamp()+"worker disconnect ("+id+") "
+					+"Cnt:"+global.procs.length
+		);
+		global.pidCount
+	});
+}
+
 function app_start(){
 	root.app.log("app.start() executing.")
 
@@ -22,106 +126,13 @@ function app_start(){
 					root.process.logProcess(root.app.log,id,process.pid,child.pid);
 					pidFile.createNew(child.pid);
 					child.send(root.ipc.message.startWorker());
-										
-					child.on('uncaughtException', function(err) {
-						root.error.raise(
-							 root.error.messages.app.start.uncaughtException,
-							 "pid:"+child.pid+",error:"+err
-						);
-						child.send(root.ipc.message.childSuicide());
-					})
-					
-					child.on('message',function(msg){
-						if(!(root.modules.core.messages.isValidMsg(msg))){
-							root.error.raise(root.error.messages.app.start.invalidMessageEncountered);
-						}
-						switch(msg.code){
-							case root.ipc.code.workerAlive:
-								child.send(
-									root.ipc.message.configureWorker(
-										child.pid,
-										id,
-										root.config.service.data.serverType,
-										config,
-										root.config.service.data.ssl.private_key,
-										root.config.service.data.ssl.public_key,
-										root.config.service.data.ssl.ca_cert
-									)
-								);
-								/* Update statistics. */
-								break;
-								
-							case root.ipc.code.workerOnline:
-								/* Update statistics. */
-								break;
-								
-							case root.ipc.code.workerFailed:
-								/* Update statistics. */
-								break;
-								
-							case root.ipc.code.workerPingReply:
-								delay=(new Date()).getTime()/1000 - msg.data;
-								if(delay < root.config.service.data.monitor.heartbeat.threshold){
-									log.write("root.ipc.code.workerPingReply w#"+id+":good");
-									this.pollMonitoring(msg);
-								}else{
-									log.write("root.ipc.code.workerPingReply w#"+id+":slow");
-									this.pollStatistics(msg);
-								}
-								break;
-							
-							case root.ipc.code.workerStatsReply:
-								log.write("root.ipc.code.workerStatsReply not impl.");
-								break;
-							
-							case root.ipc.code.childSuicideRequest:
-								child.kill('SIGKILL');
-								break;
-								
-							case root.ipc.code.childKillReply:
-								child.kill('SIGKILL');
-								break;
-				
-							default:
-								root.error.raise(root.error.messages.invalidMessageEncountered);
-								break;
-						}
-		  			});
+					setChildProcessUncaughtExceptionEvent(child);
+					setChildMessageEvent(child);		
+					setChildErrorEvent(child);
+					setChildExitEvent(child);
+					setChildCloseEvent(child);
+					setChildDisconnectEvent(child);
 
-					child.on('error',function(m){
-						if(!validator.isValidError(m)){ 
-							throw new Error(E_INV_MSG_ON_ERROR_EVENT);
-						}
-						throw new Error(
-											E_FEATURE_NOT_IMPLEMENTED
-											+":worker.on("+m+")"
-						);
-					});
-					console.log(timestamp()+"++setup exit listener");
-					child.on('exit',function(code,signal){
-						console.log(
-							 timestamp()
-							+"worker exit ("+id+")("+code+","+signal+") "
-							+"count:"+global.procs.length
-						);
-					});
-					console.log(timestamp()+"++setup close listener");
-					child.on('close',function(code,signal){
-						console.log(
-							 timestamp()
-							+"worker close ("+id+")("+code+","+signal+") "
-							+"count:"+global.procs.length
-						);
-						global.pidCount--;
-					});
-					console.log(timestamp()+"++setup disconnect listener");
-					child.on('disconnect',function(){
-						console.log(
-									 timestamp()+"worker disconnect ("+id+") "
-									+"Cnt:"+global.procs.length
-						);
-						global.pidCount
-					});
 					console.log(
 								 timestamp()
 								+"end of worker init. Cnt:"+global.procs.length
@@ -136,6 +147,5 @@ function app_start(){
 				}
 			}
 		);
-		log.list_pids();
-		return (global.procs.length>0)?true:false;
-	},
+		return (root.process.pool.length>0)?true:false;
+}
