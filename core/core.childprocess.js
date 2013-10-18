5ae590298cd7229037a32e45130a9baae51c1d21
x684867 <mail@samcaldwell.net>
2013-10-18 18:30:06 -0500

    Refactoring and debugging.

--- core/core.childprocess.js
+++ core/core.childprocess.js
@@ -9,51 +9,53 @@ module.exports=child_process_init;
 function child_process_init(){
 	root.process.child={};
 	root.process.child.eventinit={
-		uncaughtExceptionEvent=function(c){
+		uncaughtExceptionEvent:function(c){
 			c.on('uncaughtException', function(e) {
-				root.error.raise(
-					root.error.messages.app.start.uncaughtException,
-					"pid:"+c.pid+",error:"+e
-				);
+				root.error.raise(root.error.messages.app.start.uncaughtException,"pid:"+c.pid+",error:"+e);
 				c.send(root.ipc.message.childSuicide());
 			});
 		},
-		messageEvent=function(c){
-			c.on('message',function(msg){
-				if(!(root.modules.core.messages.isValidMsg(msg))){
-					root.error.raise(root.error.messages.app.start.invalidMessageEncountered);
-				}
-				switch(msg.code){
-					case root.ipc.code.workerAlive:
-						c.send(
-							root.ipc.message.configureWorker(
-								c.pid,
-								id,
-								root.config.service.data.serverType,
-								config,
-								root.config.service.data.ssl.private_key,
-								root.config.service.data.ssl.public_key,
-								root.config.service.data.ssl.ca_cert
-							)
-						);
-						/* Update statistics. */
-						break;
+		messageEvent:function(c){c.on('message',function(msg){processChildMessagesRecd(msg);});},
+		errorEvent:function(c){c.on('error',function(m){root.error.raise(root.error.messages.w Error(E_INV_MSG_ON_ERROR_EVENT));});},
+		exitEvent:function(c){c.on('exit',function(code,signal){process.deleteChildFromPool(id)},
+	}
+}
+
+function processChildMessagesRecd(msg){
+	if(!(root.modules.core.messages.isValidMsg(msg))){
+		root.error.raise(root.error.messages.app.start.invalidMessageEncountered);
+	}
+	switch(msg.code){
+		case root.ipc.code.workerAlive:
+			c.send(
+				root.ipc.message.configureWorker(
+					c.pid,
+					id,
+					root.config.service.data.serverType,
+					config,
+					root.config.service.data.ssl.private_key,
+					root.config.service.data.ssl.public_key,
+					root.config.service.data.ssl.ca_cert
+				)
+			);
+			/* Update statistics. */
+			break;
 				
-					case root.ipc.code.workerOnline:
-						/* Update statistics. */
-						break;
-				
-					case root.ipc.code.workerFailed:
-						/* Update statistics. */
-						break;
-				
-					case root.ipc.code.workerPingReply:
-						delay=(new Date()).getTime()/1000 - msg.data;
-						if(delay < root.config.service.data.monitor.heartbeat.threshold){
-							log.write("root.ipc.code.workerPingReply w#"+id+":good");
-							this.pollMonitoring(msg);
-						}else{
-							log.write("root.ipc.code.workerPingReply w#"+id+":slow");
+		case root.ipc.code.workerOnline:
+			/* Update statistics. */
+			break;
+	
+		case root.ipc.code.workerFailed:
+			/* Update statistics. */
+			break;
+	
+		case root.ipc.code.workerPingReply:
+			delay=(new Date()).getTime()/1000 - msg.data;
+			if(delay < root.config.service.data.monitor.heartbeat.threshold){
+				log.write("root.ipc.code.workerPingReply w#"+id+":good");
+				this.pollMonitoring(msg);
+			}else{
+			log.write("root.ipc.code.workerPingReply w#"+id+":slow");
 							this.pollStatistics(msg);
 						}
 						break;
@@ -74,11 +76,3 @@ function child_process_init(){
 						root.error.raise(root.error.messages.invalidMessageEncountered);
 						break;
 				}
-			});
-		},
-		errorEvent=function(c){
-			c.on('error',function(m){root.error.raise(root.error.messages.w Error(E_INV_MSG_ON_ERROR_EVENT));});},
-		exitEvent=function(c){c.on('exit',function(code,signal){process.deleteChildFromPool(id)},
-	}
-}
-
