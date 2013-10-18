d79cedd0fd737ef89ce583340bdae44f6bf4329d
x684867 <mail@samcaldwell.net>
2013-10-18 11:57:34 -0500

    Refactoring start() and associated files.

--- core/core.types.js
+++ core/core.types.js
@@ -11,17 +11,17 @@ function core_types(){
 	
 	root.type={
 	
-		tobj:'object';
-		tstr:'string';
-		tnum:'number';
-		tbool:'boolean';
-		tfunc:'function';
+		tobj:'object',
+		tstr:'string',
+		tnum:'number',
+		tbool:'boolean',
+		tfunc:'function',
 	
-		isArray:function(o){	return root.type.isObject(o) && root.type.isFunction(0);}
-		isBoolean:function(o){	return (typeof(o)=='boolean')?true:false;				}
-		isFunction:function(o){	return (typeof(o)=='function')?true:false;				}
-		isNumber:function(o){	return (typeof(o)=='number')?true:false;				}
-		isObject:function(o){	return (typeof(o)=='object')?true:false;				}
+		isArray:function(o){	return root.type.isObject(o) && root.type.isFunction(0);},
+		isBoolean:function(o){	return (typeof(o)=='boolean')?true:false;				},
+		isFunction:function(o){	return (typeof(o)=='function')?true:false;				},
+		isNumber:function(o){	return (typeof(o)=='number')?true:false;				},
+		isObject:function(o){	return (typeof(o)=='object')?true:false;				},
 		isString:function(o){	return (typeof(o)=='string')?true:false;				}
 	
 	}
