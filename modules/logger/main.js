d6240c2fb035a9f539835c3913ea873cfdf191ce
x684867 <mail@samcaldwell.net>
2013-10-20 10:24:09 -0500

    Updating with latest framework fixes.

--- modules/logger/main.js
+++ modules/logger/main.js
@@ -8,47 +8,71 @@
 	Currently only TCP connections are supported.  This will go production with 
 	encrypted communications over TLS (TCP).
 */
-
 module.exports=init;
 
 require('util');
-
+/*
+*/
 function init(source,pid,options){
-	if(typeof(source)!='string') root.error.raise(root.error.logger.invalidSourceInput);
-
+	if(!root.types.isString(source))
+		root.error.raise(root.error.logger.init.invalidSourceInput);
+	if(!root.types.isNumber(pid)) 
+		root.error.raise(root.error.logger.init.invalidPID);
+	if((!root.types.isUndefined(options)) && (!root.types.isObject(options)))
+		root.error.raise(root.error.logger.init.invalidOptions);
+	
+	var hasConsole=(isBoolean(options.hasConsole) && (options.hasConsole))?true:false;
+	
+	var hasSyslog=(isBoolean(options.hasSyslog) && (options.hasSyslog))?true:false;
+	
+	c=hasConsole?new ConsoleWriter():undefined,
+	s=hasSyslog?new SyslogWriter():undefined
+	
+	var logWriter=new writerHead(c,s);
+	logWriter.banner(['log initializing','source:'+source+' process PID:'+pid]);
+	return logWriter;
 }
-	
-	root.modules.logger.screen={
-	
-		print:function(text){
-			util.log(
-				"["+source+":"+pid+"]"+text
-			);
-		},
-		
-		line:function(){
-			util.log(
-				Array(
-					root.config.logger.width).join("-")
-				);
-		},
-		
-		doubleLine:function(){
-			util.log(
-				Array(root.config.logger.width).join("=")
-			);
-		},
+/*
+*/
+function ConsoleWriter(){
+	this.write=function(timestamp,text){console.log('['+timestamp']:'+text);};
+}
+/*
+*/
+function SyslogWriter(){
+	this.write=function(timestamp,text){/*Not implemented*/};
+}
+/*
+*/
+function writerHead(consoleWriter,syslogWriter){
 
-		banner:function(text){
-				console.log(" ");
-				logger.screen.doubleLine();
-				logger.screen.print(text);
-				logger.screen.doubleLine();
-				console.log(" ");
-		}	
-	};
-	this.write=function(m){util.log(m);};
-
+	this.write=function(text){
+		var timestamp=(new Date).toISOString();
+		if(consoleWriter) consoleWriter.write(timestamp,text);
+		if(syslogWriter)  syslogWriter.write(timestamp,text);
+	}
+	
+	this.drawLine=function(){
+		var lineText=Array(root.config.logger.width).join('-');
+		this.write(lineText);
+	}
+	
+	this.drawDoubleLine=function(){
+		var lineText=Array(root.config.logger.width).join('=');
+		this.write(lineText);
+	}
+	
+	this.banner=function(text){
+		this.drawLine();
+		if(isArray(text)){
+			text.forEach(function(d,i,a){
+				this.write("("+i+")"+d);
+			});
+		}else{
+			this.write(text);
+		}
+		this.drawDoubleLine();
+	}
 	this.error=function(errorJSON,errorObj){
 		this.write("["+errorJSON.code+"]:"+errorJSON.message);
 		if(data.type.isObject(errorObj)){
@@ -59,6 +83,5 @@ function init(source,pid,options){
 			);
 		}
 	}
-	
 }
 
