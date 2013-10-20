fc805d951ff5d5b21c46c1092a94f82de3564ff5
x684867 <mail@samcaldwell.net>
2013-10-20 14:15:49 -0500

    Updating with latest framework fixes.

---
+++ modules/broker/main.js
@@ -0,0 +1,15 @@
+/*
+	Nemesis Broker Server Module
+	/srv/nemesis/modules/broker/main.js
+	(c) 2013 Sam Caldwell.  All Rights Reserved.
+	
+*/
+module.exports=init;
+
+function init(){
+
+	console.log("Broker Server not implemented yet.");
+	console.log("see "+module.filename);
+	process.exit(1);
+
+}
