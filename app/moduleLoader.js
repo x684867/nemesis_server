024f8a6116e03db26183074d103e2f4d753148f8
x684867 <mail@samcaldwell.net>
2013-10-20 17:50:49 -0500

    Updating with latest framework fixes.

--- app/moduleLoader.js
+++ app/moduleLoader.js
@@ -10,31 +10,32 @@ module.exports=init;
 /*
  */
 function init(){
-	if(typeof(root.config)!='object'){throw new Error('root.config not defined as object');}
-	if(typeof(root.config.app)!='object'){throw new Error('root.config.app not defined as object');}
-	if(typeof(root.config.app.modules)!='string'){throw new Error('root.config.app.modules not defined as string');}
-	if( !require('fs').statSync(root.config.app.modules).isDirectory() ){throw new Error('root.config.app.modules is not a valid directory');}
+	if(typeof(root.config)!='object') throw new Error('root.config not defined as object');
+	if(typeof(root.config.app)!='object') throw new Error('root.config.app not defined as object');
+	if(typeof(root.config.app.modules)!='string') throw new Error('root.config.app.modules not defined as string');
+	if( !require('fs').statSync(root.config.app.modules).isDirectory() ) throw new Error('root.config.app.modules is not a valid directory');
 	root.modules={};
-	root.modules.load=function(modName){	console.log('loading '+modName);modInspect(modName,'noPreload');	}
-	root.modules.preload=function(modName){	console.log('preloading '+modName);modInspect(modName,'preload');	}
+	root.modules.load=function(modName){	modName);modInspect(modName,'standard');	}
+	root.modules.preload=function(modName){	modInspect(modName,'preload');	}
 	root.modules.loadall=function(){
 		var fs=require('fs');
 		console.log(Array(80).join('='));
-		console.log('loading all non-preload modules in ('+root.config.app.modules+').');
+		console.log('loading standard loadTime modules in ('+root.config.app.modules+').');
 		console.log(Array(80).join('-'));
 		require('fs').readdirSync(root.config.app.modules).forEach(
 			function(m,i,a){
-					console.log('loadall() is loading '+m);
-					modInspect(m,'noPreload');
+					console.log('loadall() is opening...'+m);
+					modInspect(m,'standard');
 			}
 		);
 	}
 }
 /*
  */
-function modInspect(modName,context){
-
+function modInspect(modName,loadTime){
 	fs=require('fs');
+	
+	if( (loadTime!='preload') && (loadTime!='standard') ) throw new Error('invalid loadTime parameter passed to modInspect()');
 
 	if(typeof(root.modules[modName])!='undefined'){
 		console.log('module ['+modName+'] exists.  Cannot load duplicate.');
@@ -46,7 +47,7 @@ function modInspect(modName,context){
 	root.modules[modName]={}
 	var module_path=root.config.app.modules+modName+"/";
 	
-	if( !fs.statSync(module_path).isDirectory() ) throw new Error('module ('+modName+') not found');
+	if( !fs.statSync(module_path).isDirectory() ) throw new Error('module ('+modName+') not found.  Check ('+module_path+')');
 
 	var module_manifest=module_path+"manifest.json";
 	if( fs.statSync(module_manifest).isFile() ) {
@@ -59,35 +60,16 @@ function modInspect(modName,context){
 	}else{
 		throw new Error('manifest.json not found');
 	}
-	/*Validate manifest JSON*/
-	if(typeof( root.modules[modName].manifest )!='object' )
-		throw new Error('Invalid manifest detected for module '+modName);
-	if(typeof( root.modules[modName].manifest.name )!='string' )
-		throw new Error('Invalid manifest name (expected string)  module:'+modName);
-	if(typeof( root.modules[modName].manifest.group )!='string' )
-		throw new Error('Invalid manifest group (expected string) module:'+modName);
-	if(typeof( root.modules[modName].manifest.loader )!='object' )
-		throw new Error('Invalid manifest loader object.  module:'+modName);
-	if(typeof( root.modules[modName].manifest.loader.bootstrap )!='boolean' )
-		throw new Error('Invalid manifest bootstrap (expect boolean).  module:'+modName);
-	if(typeof( root.modules[modName].manifest.loader.preload )!='boolean' )
-		throw new Error('Invalid manifest preload (expected boolean).  module:'+modName);
-	if(typeof( root.modules[modName].manifest.main )!='string' )
-		throw new Error('Invalid manifest main (expected string).  module:'+modName);
-	if(typeof( root.modules[modName].manifest.config )!='string' )
-		throw new Error('Invalid manifest config (expected string).  module:'+modName);
-	if(root.modules[modName].manifest.loader.preload){
-		if(context.toLowerCase()=='preload'){
-			load_my_module(modName);		
-		}else{
-			console.log('Not loading module [' + modName + '] not marked as preload.');
-		}
+	
+	if (isManifestValid(root.modules[modName].manifest)&&(root.modules[modName].manifest.loader.loadTime==loadTime)){
+			if(loadTime=='preload') load_my_module(modName);
+			if(loadTime=='standard') load_my_module(modName);
 	}else{
-		if(context.toLowerCase()=='nopreload'){
-			load_my_module(modName);
-		}else{
-			console.log('Not loading module [' + modName + '] marked as preload.');
-		}
+		console.log(
+		     '     Not loading module [' + modName + '].'
+			+'       Module loadTime=' + root.modules[modName].manifest.loader.loadTime 
+			+'       Expected '+loadTime
+		);
 	}
 	var error_file=module_path+"errors-"+process.env.LANG+".json"
 	if( fs.statSync(error_file ) ){
@@ -147,4 +129,33 @@ function load_my_module(modName){
 	/*
 		Add more objects from the manifest here.
 	 */
+}
+
+function isManifestValid(manifest){
+	/*Validate manifest JSON*/
+	if(typeof( manifest )=='object' )
+		if(typeof( manifest.name )=='string' )
+			if(typeof( manifest.group )=='string' )
+				if(typeof( manifest.main )=='string' )
+					if(typeof( manifest.config )=='string' )
+						if(typeof( manifest.loader )=='object' )
+							if(typeof( manifest.loader.loadTime )=='string' )
+								if((manifest.loader.loadTime=='standard') || (manifest.loader.loadTime=='preload')))
+									return true;
+								else
+									throw new Error('Invalid loader.loadTime value.  Expected {"preload" or "standard"}.  module:'+modName);
+							else						
+								throw new Error('Invalid loader.loadTime.  Expected string.  module:'+modName);
+						else
+							throw new Error('Invalid manifest loader object.  module:'+modName);
+					else
+						throw new Error('Invalid manifest config (expected string).  module:'+modName);
+				else
+					throw new Error('Invalid manifest main (expected string).  module:'+modName);
+			else
+				throw new Error('Invalid manifest group.  Expected string. module:'+modName);
+		else
+			throw new Error('Invalid manifest name.  Expected string.  module:'+modName);
+	else
+		throw new Error('Invalid manifest detected for module.  Expected object.'+modName);
 }
