51869d241890e247f40cc53c8fe5cc7d3c74c2e4
x684867 <mail@samcaldwell.net>
2013-10-26 21:01:06 -0500

    Added logger.js functionality.  Added loggerClass.js.  Added priorities and facilities for SYSLOG based on the latest RFC specifications to the types package so that we can add type-checking functionality.

--- packages/logger/main.js
+++ packages/logger/main.js
@@ -7,7 +7,6 @@
 	through console and SYSLOG by overloading the screen.log() method to 
 	tee writes to screen.log() both to the screen and to the logger's alternate
 	output streams (e.g. log file).
-	
 		
 	USE:
 		root.logger
@@ -18,66 +17,29 @@
 
 */
 {
-	init=function(source,pid){
-		if(validate_configuration()){
-			
-			/*Create the global logger if not already defined*/
-			if(root.types.isUndefined(root.logger)){
-			
-				screen.log("     global logger not found....creating [pid:"+process.pid+"]");
-				root.logger=new logger_class(root.app.title,process.pid);
-				
-			}else{
-			
-				screen.log("     global logger exists.");
-				
-			}		
-			if(root.types.isString(source) || root.types.isNumber(pid)){
-			
-				screen.log("     local logger ["+source+"] for process [pid:"+pid+"] requested.");
-				return new logger_class(source,pid);
-				
-			}else{
-			
-				error.warn("Invalid inputs...diagnosing....");
-				if(!root.types.isString(source))
-					raise(root.errors.logger.init.missingInput.Source);
-				else
-					raise(root.errors.logger.init.missingInput.PID);
-			}
-			/* */
-		}else{
-		
-			root.error.warn("Invalid package configuration file...diagnosing....");
-			
-			switch(validate_configuration){
-				case messages.logger.validate.codes.invalidConfig.syslog:
-					errors.raise(errors.logger.init.invalidConfig.syslog);break;
-				case messages.logger.validate.codes.invalidConfig.syslogIp:
-					errors.raise(errors.logger.init.invalidConfig.syslogIp);break;
-				case messages.logger.validate.codes.invalidConfig.syslogPort:
-					errors.raise(errors.logger.init.invalidConfig.syslogPort);break;
-				case messages.logger.validate.codes.invalidConfig.syslogTLS:
-					errors.raise(errors.logger.init.invalidConfig.syslogTLS);break;
-				case messages.logger.validate.codes.invalidConfig.syslogTLSkey:
-					errors.raise(errors.logger.init.invalidConfig.syslogTLSkey);break;
-				case messages.logger.validate.codes.invalidConfig.syslogTLScert:
-					errors.raise(errors.logger.init.invalidConfig.syslogTLScert);break;
-				case messages.logger.validate.codes.invalidConfig.syslogTLSca:
-					errors.raise(errors.logger.init.invalidConfig.syslogTLSca);break;
-				default:
-					errors.raise(errors.logger.init.invalidConfig.unknown);break;
-			}/*end of switch*/			
+	init=function(source,priority,facility, pid){
+		/*Validate the configuration file*/
+		switch(false){
+			case types.isObject(config.logger.syslog):			errors.raise(errors.logger.init.invalidConfig.syslog);			break;
+			case types.isString(config.logger.syslog.ip):		errors.raise(errors.logger.init.invalidConfig.syslogIp);		break;
+			case types.isNumber(config.logger.syslog.port):		errors.raise(errors.logger.init.invalidConfig.syslogPort);		break;
+			case types.isObject(config.logger.syslog.tls):		errors.raise(errors.logger.init.invalidConfig.syslogTLS);		break;
+			case types.isString(config.logger.syslog.tls.key):	errors.raise(errors.logger.init.invalidConfig.syslogTLSkey);	break;
+			case types.isString(config.logger.syslog.tls.cert):	errors.raise(errors.logger.init.invalidConfig.syslogTLScert);	break;
+			case types.isString(config.logger.syslog.tls.ca):	errors.raise(errors.logger.init.invalidConfig.syslogTLSca);		break;
+			default: return messages.logger.validate.codes.invalidConfig.unknown; break;
+		}
+		screen.log('logger configuration is valid.');
+
+		loggerClass=require('./packages/logger/loggerClass.js');
+
+		if(types.isUndefined(root.logger)){
+			overloadScreenLog();
+			root.logger=new loggerClass('app',process.pid);
+		}
+		if(	types.isString(source) && types.isNumber(pid)){ 
+			return new loggerClass(source,pid);
+			screen.log('creating local logger');
 		}
 	}
 }
-
-function logger(source.pid){
-
-
-}
-
-function validate_configuration(){
-
-}	
-
